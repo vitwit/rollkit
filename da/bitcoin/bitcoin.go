b05f9bd94fb0003ff4eb0f849b02bc21b37ee799
Javed Khan <tuxcanfly@gmail.com>
2023-02-28 05:15:40 -0800

    da: integrate rollkit-btc as bitcoin da backend

---
+++ da/bitcoin/bitcoin.go
@@ -0,0 +1,166 @@
+package bitcoin
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/gogo/protobuf/proto"
+	ds "github.com/ipfs/go-datastore"
+
+	rollkitbtc "github.com/rollkit/rollkit-btc"
+	"github.com/rollkit/rollkit/da"
+	"github.com/rollkit/rollkit/log"
+	"github.com/rollkit/rollkit/types"
+	pb "github.com/rollkit/rollkit/types/pb/rollkit"
+)
+
+// DataAvailabilityLayerClient use celestia-node public API.
+type DataAvailabilityLayerClient struct {
+	client      *rollkitbtc.Relayer
+	namespaceID types.NamespaceID
+	config      Config
+	logger      log.Logger
+}
+
+var _ da.DataAvailabilityLayerClient = &DataAvailabilityLayerClient{}
+var _ da.BlockRetriever = &DataAvailabilityLayerClient{}
+
+// Config stores Celestia DALC configuration parameters.
+type Config struct {
+	Host         string `json:"host"`
+	User         string `json:"user"`
+	Pass         string `json:"pass"`
+	HTTPPostMode bool   `json:"http_post_mode"`
+	DisableTLS   bool   `json:"disable_tls"`
+}
+
+// Init initializes DataAvailabilityLayerClient instance.
+func (c *DataAvailabilityLayerClient) Init(namespaceID types.NamespaceID, config []byte, kvStore ds.Datastore, logger log.Logger) error {
+	c.namespaceID = namespaceID
+	c.logger = logger
+
+	if len(config) > 0 {
+		return json.Unmarshal(config, &c.config)
+	}
+
+	client, err := rollkitbtc.NewRelayer(rollkitbtc.Config{
+		Host:         c.config.Host,
+		User:         c.config.User,
+		Pass:         c.config.Pass,
+		HTTPPostMode: c.config.HTTPPostMode,
+		DisableTLS:   c.config.DisableTLS,
+	})
+	if err != nil {
+		return err
+	}
+
+	c.client = client
+
+	return nil
+}
+
+// Start prepares DataAvailabilityLayerClient to work.
+func (c *DataAvailabilityLayerClient) Start() error {
+	c.logger.Info("starting Bitcoin Client", "Host", c.config.Host)
+	var err error
+	return err
+}
+
+// Stop stops DataAvailabilityLayerClient.
+func (c *DataAvailabilityLayerClient) Stop() error {
+	c.logger.Info("stopping Celestia Data Availability Layer Client")
+	return nil
+}
+
+// SubmitBlock submits a block to DA layer.
+func (c *DataAvailabilityLayerClient) SubmitBlock(ctx context.Context, block *types.Block) da.ResultSubmitBlock {
+	blob, err := block.MarshalBinary()
+	if err != nil {
+		return da.ResultSubmitBlock{
+			BaseResult: da.BaseResult{
+				Code:    da.StatusError,
+				Message: err.Error(),
+			},
+		}
+	}
+
+	hash, err := c.client.Write(blob)
+
+	if err != nil {
+		return da.ResultSubmitBlock{
+			BaseResult: da.BaseResult{
+				Code:    da.StatusError,
+				Message: err.Error(),
+			},
+		}
+	}
+
+	return da.ResultSubmitBlock{
+		BaseResult: da.BaseResult{
+			Code:    da.StatusSuccess,
+			Message: "tx hash: " + hash.String(),
+		},
+	}
+}
+
+// CheckBlockAvailability queries DA layer to check data availability of block at given height.
+func (c *DataAvailabilityLayerClient) CheckBlockAvailability(ctx context.Context, dataLayerHeight uint64) da.ResultCheckBlock {
+	data, err := c.client.Read(dataLayerHeight)
+	if err != nil {
+		return da.ResultCheckBlock{
+			BaseResult: da.BaseResult{
+				Code:    da.StatusError,
+				Message: err.Error(),
+			},
+		}
+	}
+
+	return da.ResultCheckBlock{
+		BaseResult: da.BaseResult{
+			Code:     da.StatusSuccess,
+			DAHeight: dataLayerHeight,
+		},
+		DataAvailable: len(data) > 0,
+	}
+}
+
+// RetrieveBlocks gets a batch of blocks from DA layer.
+func (c *DataAvailabilityLayerClient) RetrieveBlocks(ctx context.Context, dataLayerHeight uint64) da.ResultRetrieveBlocks {
+	data, err := c.client.Read(dataLayerHeight)
+	if err != nil {
+		return da.ResultRetrieveBlocks{
+			BaseResult: da.BaseResult{
+				Code:    da.StatusError,
+				Message: err.Error(),
+			},
+		}
+	}
+
+	blocks := make([]*types.Block, len(data))
+	for i, msg := range data {
+		var block pb.Block
+		err = proto.Unmarshal(msg, &block)
+		if err != nil {
+			c.logger.Error("failed to unmarshal block", "daHeight", dataLayerHeight, "position", i, "error", err)
+			continue
+		}
+		blocks[i] = new(types.Block)
+		err := blocks[i].FromProto(&block)
+		if err != nil {
+			return da.ResultRetrieveBlocks{
+				BaseResult: da.BaseResult{
+					Code:    da.StatusError,
+					Message: err.Error(),
+				},
+			}
+		}
+	}
+
+	return da.ResultRetrieveBlocks{
+		BaseResult: da.BaseResult{
+			Code:     da.StatusSuccess,
+			DAHeight: dataLayerHeight,
+		},
+		Blocks: blocks,
+	}
+}
